b8f6fe8bd33bd7f840836c5e79798690e7fdd61b
Brígida Oliveira <ocfb0914@hotmail.com>
2024-08-02 10:48:32 -0300

    variaveis e constantes

---
+++ variaveis_e_constantes.py
@@ -0,0 +1,15 @@
+nome = "Brígida"
+idade = "32"
+
+print(nome, idade)
+
+nome = "Laura"
+
+print(nome, idade)
+
+limite_saque_diario = 1000 #nome bom de variável
+lim_sa_di = 1000 #alternativa péssima, prefira o de cima
+
+BRAZILIAN_STATES = ["SP", "CE", "RJ"] 
+
+print(BRAZILIAN_STATES) #convenção para constante
